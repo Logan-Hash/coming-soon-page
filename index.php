5f218ff331214b3aa990a9aee35c5b048c6a5036
Pyae Phyo Thant <pyaephyothant1998@gmail.com>
2020-10-24 16:08:49 +0630

    publish on github

---
+++ index.php
@@ -0,0 +1,113 @@
+<!doctype html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="no-js" lang="">
+<!--<![endif]-->
+
+<head>
+<meta charset="utf-8">
+<meta name="author" content="webthemez">
+<meta name="description" content="">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>UMPS | The Upper Myanmar Photographic Society</title>
+<link rel="icon" href="./images/UMPS RGB PNG.png">
+<link rel="stylesheet" href="css/bootstrap.min.css">
+<link rel="stylesheet" href="css/main.css">
+<link rel="stylesheet" href="css/responsive.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+</head>
+
+<body class="banner-area"> 
+<!--header section --><div id="large-header" class="large-header">
+					<canvas id="demo-canvas"></canvas> 
+</div>
+<section class="banner" role="banner"> 
+    <!-- overlay -->
+    <div class="banner-area-gradient"></div>
+    <!-- overlay -->
+    <div class="inner-bg">
+      <div class="container">
+	  <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+          <div class="banner-text text-center"> <a class="logo" href="#"><img src="./images/umps-logo-they-give.png" alt=""></a>
+            
+			<div class="type-wrap">
+            <div id="typed-strings">
+                <p>Our Website is now <em>under</em>  construction.</p> 
+                <span>Stay Tuned For Something <strong>Amazing</strong></span>
+            </div>
+            <span id="typed" style="white-space:pre;"></span>
+        </div> 
+            <!--Countdown -->
+            <div id="countdown"></div> 
+            </p>
+          </div>
+        </div>
+		</div> 
+			  <div class="row">
+				<div class="col-md-10 col-md-offset-1">
+		
+		<!--subscribe section -->
+		<!-- <div id="subscribes" class="subscribe">  -->
+			  <!-- subscribe form -->
+			  <!-- <div class="subscribe-form"> 
+				<h2>Subscribe to our list</h2>	   
+				<form method="post" action="php/subscribe.php" name="subscribeform" id="subscribeform">
+				  <input type="text" name="email" placeholder="Enter email address" id="subemail" />
+				  <input type="submit" name="send" value="Submit" id="subsubmit" class="btn2" />
+				</form> -->
+				
+				<!-- subscribe message -->
+				<!-- <div id="mesaj"></div> -->
+				<!-- subscribe message -->  
+						<!-- </div> 
+					</div>
+				</div>
+			</div>  -->
+		<!--subscribe section --> 
+
+      <!-- </div>
+    </div> -->
+	<!-- Footer section -->
+<footer class="footer">
+  <div class="container">
+    <div class="row"> 
+      <div class="col-md-10 col-md-offset-1">
+        <ul class="footer-share">
+          <li><a href="https://www.facebook.com/UMPSmyanmar"><i class="fa fa-facebook"></i></a></li>
+          <li><a onclick="alertNo()" href="#"><i class="fa fa-twitter"></i></a></li>
+          <li><a onclick="alertNo()" href="#"><i class="fa fa-linkedin"></i></a></li>
+          <li><a onclick="alertNo()" href="#"><i class="fa fa-google-plus"></i></a></li>
+        </ul> 
+        <p> Copyright © <?php echo date('Y') ?> <a href="https://picoinnovation.com" target="_blank">by Pico Innovation</a></p>
+      </div>
+    </div>
+  </div> 
+</footer>
+<!-- Footer section -->  
+</section>
+<!--header section -->  
+ 
+<!-- JS files--> 
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script> 
+<script src="js/bootstrap.min.js"></script> 
+<script src="js/jquery.backstretch.min.js"></script> 
+<script src="js/jquery.countdown.js"></script> 
+<script type="text/javascript" src="js/jquery.subscribe.js"></script> 
+<script src="js/typed.min.js" type="text/javascript"></script>
+<script src="js/main.js"></script> 
+
+<script src="js/TweenLite.min.js"></script>
+<script src="js/EasePack.min.js"></script>
+<script src="js/rAF.js"></script>
+<script src="js/demo-1.js"></script> 
+
+<script>
+  function alertNo() {
+    alert('Dont have link !')
+  }
+</script>
+</body>
+</html>
